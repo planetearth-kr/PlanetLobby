ae2d90797ab18b84cfaef9f635582ea2d330062b
Irochi <me@irochi.moe>
2024-11-05 16:19:02 +0900

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,31 +4,40 @@ plugins {
 
 group = "kr.planetearth.plugins"
 version = "1.0-SNAPSHOT"
-description = "PlanetLobby plugin"
+description = "PlanetLobby plugin for planetearth.kr"
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.purpurmc.org/snapshots")
     maven("https://jitpack.io")
-    maven("https://nexus.scarsz.me/content/groups/public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    compileOnly("com.discordsrv:discordsrv:1.28.0")
+    //compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("org.purpurmc.purpur:purpur-api:1.20.1-R0.1-SNAPSHOT")
 }
 
 tasks {
     processResources {
-        filteringCharset = Charsets.UTF_8.name()
-        filesMatching("paper-plugin.yml") {
-            expand("version" to project.version)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(
+                "version" to project.version,
+                "description" to project.description
+            )
         }
+        filteringCharset = "UTF-8"
     }
     compileJava {
-        options.encoding = Charsets.UTF_8.name()
+        options.encoding = "UTF-8"
         options.release.set(17)
     }
 }
--- src/main/java/kr/planetearth/plugins/planetlobby/PlanetLobby.java
+++ src/main/java/kr/planetearth/plugins/planetlobby/PlanetLobby.java
@@ -3,14 +3,12 @@ package kr.planetearth.plugins.planetlobby;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import kr.planetearth.plugins.planetlobby.listeners.PlayerListener;
-import kr.planetearth.plugins.planetlobby.listeners.DiscordSRVListener;
 
 public final class PlanetLobby extends JavaPlugin {
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-        Bukkit.getPluginManager().registerEvents(new DiscordSRVListener(), this);
     }
 
     @Override
