78217413f058d2e2e0e0459b571473c7b80469f6
Irochi <me@irochi.moe>
2025-06-30 22:33:20 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,44 +2,43 @@ plugins {
     java
 }
 
-group = "kr.planetearth.plugins"
-version = "1.0-SNAPSHOT"
-description = "PlanetLobby plugin for planetearth.kr"
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
+group = "kr.planetearth"
+version = "1.0"
 
 repositories {
     mavenCentral()
-    mavenLocal()
-    maven("https://repo.viaversion.com")
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.purpurmc.org/snapshots")
-    maven("https://jitpack.io")
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "viaversion"
+        url = uri("https://repo.viaversion.com")
+    }
 }
 
 dependencies {
-    //compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    compileOnly("org.purpurmc.purpur:purpur-api:1.20.1-R0.1-SNAPSHOT")
-    compileOnly("com.viaversion:viaversion-api:5.3.1")
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("com.viaversion:viaversion-api:5.4.0")
 }
 
-tasks {
-    processResources {
-        filteringCharset = "UTF-8"
-        filesMatching("plugin.yml") {
-            expand(
-                "version" to project.version,
-                "description" to project.description
-            )
-        }
-        filteringCharset = "UTF-8"
-    }
-    compileJava {
-        options.encoding = "UTF-8"
-        options.release.set(17)
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
     }
 }
