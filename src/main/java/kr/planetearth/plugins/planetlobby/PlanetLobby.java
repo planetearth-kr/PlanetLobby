6975b879beb94adba4e04c57865807761ab55847
Irochi <me@irochi.moe>
2025-07-04 13:06:51 +0900

    Remove unused commands

--- src/main/java/kr/planetearth/plugins/planetlobby/PlanetLobby.java
+++ src/main/java/kr/planetearth/plugins/planetlobby/PlanetLobby.java
@@ -1,12 +1,8 @@
 package kr.planetearth.plugins.planetlobby;
 
-import kr.planetearth.plugins.planetlobby.command.PermissionGivePerUUIDCommand;
-import kr.planetearth.plugins.planetlobby.command.RankRewardCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import kr.planetearth.plugins.planetlobby.listeners.PlayerListener;
-
-import java.util.Objects;
 
 public final class PlanetLobby extends JavaPlugin {
     private static PlanetLobby instance;
@@ -15,15 +11,7 @@ public final class PlanetLobby extends JavaPlugin {
     public void onEnable() {
         instance = this;
 
-        if (!getDataFolder().exists()) {
-            if (!getDataFolder().mkdirs()) {
-                getLogger().warning("Failed to create plugin data folder: " + getDataFolder().getPath());
-            }
-        }
-
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-        Objects.requireNonNull(getCommand("랭크보상")).setExecutor(new RankRewardCommand());
-        Objects.requireNonNull(getCommand("PermissionGivePerUUID")).setExecutor(new PermissionGivePerUUIDCommand());
     }
 
     @Override
