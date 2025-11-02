c7c4eb01b4042e9abfed8afb6cacf31b03b203a1
Irochi <me@irochi.moe>
2025-11-02 13:50:31 +0900

    Update package & Add InventoryFramework dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
     java
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
-group = "kr.planetearth"
-version = "1.0"
+group = providers.gradleProperty("group").get()
+version = providers.gradleProperty("version").get()
+description = providers.gradleProperty("description").get()
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
 
 repositories {
     mavenCentral()
@@ -12,25 +20,58 @@ repositories {
     maven("https://repo.viaversion.com/")
 }
 
+configurations.all {
+    exclude(group = "org.bukkit", module = "bukkit")
+
+    resolutionStrategy {
+        force("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    }
+}
+
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    compileOnly("com.viaversion:viaversion-api:5.4.0")
-    compileOnly("net.luckperms:api:5.4")
+    compileOnly("com.viaversion:viaversion-api:5.5.1")
+    compileOnly("net.luckperms:api:5.5")
+    implementation("com.github.stefvanschie.inventoryframework:IF:0.11.5")
 }
 
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-}
+tasks {
+    processResources {
+        val projectVersion = version
+        val projectDescription = description
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(
+                mapOf(
+                    "version" to projectVersion,
+                    "description" to projectDescription
+                )
+            )
+        }
+    }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-}
+    jar {
+        manifest {
+            attributes["paperweight-mappings-namespace"] = "mojang"
+        }
+    }
 
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+    shadowJar {
+        relocate("com.github.stefvanschie.inventoryframework", "kr.planetearth.plugins.inventoryframework")
+
+        manifest {
+            attributes["paperweight-mappings-namespace"] = "mojang"
+        }
+
+        archiveClassifier.set("")
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(21)
+    }
+
+    build {
+        dependsOn(shadowJar)
     }
 }
